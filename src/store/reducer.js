41046ae3dc99889a2b1dbea1e7e2d0973b487527
Rachita Upreti <rachita.upreti@gmail.com>
2019-04-25 12:20:04 +0300

    Redux Counter APP

---
+++ src/store/reducer.js
@@ -0,0 +1,34 @@
+const initialState = {
+    counter:0
+}
+
+const reducer = (state = initialState , action) =>{
+    if(action.type==='INCREMENT'){
+        return {
+            ...state,
+            counter:state.counter+1
+        };
+    }
+    if(action.type==='DECREMENT'){
+        return {
+            ...state,
+            counter:state.counter-1
+        };
+    }
+    if(action.type==='INCREMENT_FIVE'){
+        return {
+            ...state,
+            counter:state.counter+5
+        };
+    }
+    if(action.type==='DECREMENT_FIVE'){
+        return {
+            ...state,
+            counter:state.counter-5
+        };
+    }
+    return state
+};
+
+
+export default reducer ;
